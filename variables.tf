54ab9cd92ed188ff74887f555b4365d6c4f3e3f4
peidong hu <peidong.hu@commscope.com>
2020-08-14 05:56:58 +0000

    add replacement tag for vars

--- variables.tf
+++ variables.tf
@@ -13,25 +13,25 @@ variable "aws_amis" {
 
 variable "us-east-2b-subnet-ids" {
   type = list(string)
-  default = ["subnet-d3c3b7a9"]
+  default = ["subnet-d3c3b7a9"] #US-EAST-2B-SUBNET-IDS
 }
 
 variable "us-east-2c-subnet-ids" {
   type = list(string)
-  default = ["subnet-9ef12ed2"]
+  default = ["subnet-9ef12ed2"] #US-EAST-2C-SUBNET-IDS
 }
 
 variable "security-groups-template" {
   type = list(string)
-  default = ["sg-9301b6fd"]
+  default = ["sg-9301b6fd"]  #SG-IN-TEMPLATE
 }
 
 variable "ins-type" {
-  default = "t3.micro"
+  default = "t3.micro" #INS_TYPE
 }
 
 variable "ami-id" {
-  default = "ami-007e9fbe81cfbf4fa"
+  default = "ami-007e9fbe81cfbf4fa" #AMI-ID
 }
 
 variable "bzone-total" {
@@ -39,22 +39,22 @@ variable "bzone-total" {
 }
 
 variable "bzone-spot-count" {
-  default = 4
+  default = 4 #BZONE-SPOT-COUNT
 }
 
 variable "bzone-ondemand-count" {
-  default = 1
+  default = 1 #BZONE-ONDEMAND-COUNT
 }
 
 variable "czone-total" {
-  default = 5
+  default = 5 #CZONE-TOTAL
 }
 
 variable "czone-spot-count" {
-  default = 4
+  default = 4 #CZONE-SPOT-COUNT
 }
 
 variable "czone-ondemand-count" {
-  default = 1
+  default = 1 #CZONE-ONDEMAND-COUNT
 }
 
